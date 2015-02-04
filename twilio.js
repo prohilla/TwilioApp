4c381ee7a9cecbcf3171b8afedbb11483108146f
prashant rohilla <Prashant@prashants-MacBook-Pro.local>
2015-02-04 14:24:16 -0800

    lol

--- twilio.js
+++ twilio.js
@@ -5,10 +5,10 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
+app.get('/input', function(req, res) {
 	var twiml = new twilio.TwimlResponse();
     twiml.say('Welcome to Peace Leaders')
-    .gather({ action:'https://secure-hollows-8464.herokuapp.com',
+    .gather({ action:'https://secure-hollows-8464.herokuapp.com/input',
         method:'GET'
     }, function() {
         this.say('Press 1 to hear Pope Francis latest tweet ')
