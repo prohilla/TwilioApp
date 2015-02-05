50109e439599f216344ad2255b4ebe41b9caeedf
prashant rohilla <Prashant@prashants-MacBook-Pro.local>
2015-02-04 17:42:10 -0800

    combine

--- twilio.js
+++ twilio.js
@@ -13,7 +13,7 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-    res.sendfile(__dirname + '/html/NewFile.html');
+    res.sendfile(__dirname + '/gumba/index.html');
 });
 
 app.get('/call', function(req, res) {
